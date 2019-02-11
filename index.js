cce6e641ce04a964ea82af7682d12c99b0df9001
Juuso Nykänen <juusonykanen@hotmail.com>
2019-02-11 13:24:14 -0800

    ex 3.14 working

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Person = require('./models/person');
 
-app.use(express.static('build'))
+app.use(express.static('build'));
 app.use(cors());
 app.use(bodyParser.json());
-
-const url =  
-`mongodb+srv://puhelinluettelo_mongo_user:${password}@cluster0-zo7xf.mongodb.net/numerotDB?retryWrites=true`;
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
-});
-
-personSchema.set('toJSON', { 
-  transform: (document, returnObject) => {
-    returnObject.id = returnObject._id.toString();
-    delete returnObject._id;
-    delete returnObject.__v;
-  }
-});
-
-const Person = mongoose.model('Person', personSchema);
 
 morgan.token('reqBody', function requestBody (req) {
   if(req.method === 'POST' && req.headers["content-type"] === 'application/json') {
@@ -33,25 +17,9 @@ morgan.token('reqBody', function requestBody (req) {
   }
   return null;
 });
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'));
 
-let persons = [
-  {
-    "name": "Lea Kutvonen",
-    "number": "0000000000",
-    "id": 4
-  },
-  {
-    "name": "Kalle Kaalinen",
-    "number": "0109998888",
-    "id": 6
-  },
-  {
-    "name": "Kirsti Kaalinen",
-    "number": "1112223333",
-    "id": 7
-  }
-];
 
 app.get('api/', (request, response) => {
   response.send('<h1>Persons API</h1>');
@@ -66,54 +34,57 @@ app.get('api/info', (request, response) => {
 });
 
 app.get('/api/persons', (request, response) => {
-  mongoose.connect(url, {useNewUrlParser: true});
   Person.find({})
   .then(result => {
-    response.json(result.map(person => person.toJSON()))
+    response.json(result.map(person => person.toJSON()));
   })
   .then(() => mongoose.connection.close());
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const person = persons.find(person => person.id === Number(request.params.id));
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id).then(person => {
+    response.json(person.toJSON())
+  });
 });
 
 app.post('/api/persons', (request, response) => {
-  const person = request.body;
+  const body = request.body;
 
-  if (person.name === undefined) { 
-    return response.status(400).json({ error: 'name missing' });
-  }
-  
-  if (person.number === undefined) { 
-    return response.status(400).json({ error: 'number missing' });
+  if (body.name === undefined) { 
+    return response.status(400).json({ error: 'Name missing' });
   }
 
+  if (body.number === undefined) { 
+    return response.status(400).json({ error: 'Number missing' });
+  }
+
+  /*
   const exists = persons.find(element => element.name === person.name);
 
   if (exists) {
     return response.status(409).json({ 
       error:  `Entry already exists for ${person.name}` });
   }
+  */
 
-  person.id = ~~(Math.random() * 1000000) + 1;
+ const person = new Person({
+  name: body.name,
+  number: body.number,
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
-
+  person.save().then(createdPerson => {
+    console.log('created Person!!!!!', createdPerson)
+    response.json(createdPerson.toJSON());
+  });
+  
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  persons = persons.filter(person => person.id !== Number(request.params.id));
-  response.status(204).end();
+  //persons = persons.filter(person => person.id !== Number(request.params.id));
+  //response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
