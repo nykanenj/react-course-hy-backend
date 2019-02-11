ff1f188a994c7d4a1bba4ea6ad80de54eb5552f2
Juuso Nykänen <juusonykanen@hotmail.com>
2019-02-11 06:40:17 -0800

    front added

---
+++ build/precache-manifest.139c54b24a27ae3134a888fd9abcdb2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13bde3cc1f4417520c8a",
+    "url": "/static/js/main.13bde3cc.chunk.js"
+  },
+  {
+    "revision": "45a33658116e915bff91",
+    "url": "/static/js/1.45a33658.chunk.js"
+  },
+  {
+    "revision": "13bde3cc1f4417520c8a",
+    "url": "/static/css/main.acbdc185.chunk.css"
+  },
+  {
+    "revision": "423540f54cf281170cd9e5c583462d52",
+    "url": "/index.html"
+  }
+];
