9bba817989b83ede73ea6a73382bce2a2d04cccd
Juuso Nykänen <juusonykanen@hotmail.com>
2019-02-17 09:43:56 -0800

    eslint changes and changed info functionality

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+	"no-console": 0,
+	"prefer-destructuring": 0,
+	"consistent-return": 0,
+    }
+};
--- index.js
+++ index.js
@@ -1,60 +1,67 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const Person = require('./models/person');
 
+const app = express();
 app.use(express.static('build'));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'));
 
-morgan.token('reqBody', function requestBody (req) {
-  if(req.method === 'POST' && req.headers["content-type"] === 'application/json') {
+morgan.token('reqBody', (req) => {
+  if (req.method === 'POST' && req.headers['content-type'] === 'application/json') {
     return JSON.stringify(req.body);
   }
   return null;
 });
 
-app.get('api/', (request, response) => {
+app.get('/api/', (request, response) => {
   response.send('<h1>Persons API</h1>');
 });
 
-app.get('api/info', (request, response) => {
+app.get('/api/info', (request, response, next) => {
+  Person.find({})
+    .then((result) => {
+      response.send(
+        `<div>Puhelinluettelossa ${result.length} henkilön tiedot</div>
+        <br />
+        <div>${new Date()}</div>`,
+      );
+    })
+    .catch(error => next(error));
   response.send(
-    `<div>Puhelinluettelossa ${persons.length} henkilön tiedot</div>
-    <br />
-    <div>${new Date}</div>`
+
   );
 });
 
 app.get('/api/persons', (request, response, next) => {
   Person.find({})
-  .then(result => {
-    response.json(result.map(person => person.toJSON()));
-  })
-  .catch(error => next(error));
+    .then((result) => {
+      response.json(result.map(person => person.toJSON()));
+    })
+    .catch(error => next(error));
 });
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    response.json(person.toJSON())
-  })
-  .catch(error => next(error));
+    .then((person) => {
+      response.json(person.toJSON());
+    })
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body;
+  const { body } = request;
 
-  if (body.name === undefined) { 
+  if (body.name === undefined) {
     return response.status(400).json({ error: 'Name missing' });
   }
 
-  if (body.number === undefined) { 
+  if (body.number === undefined) {
     return response.status(400).json({ error: 'Number missing' });
   }
 
@@ -64,35 +71,33 @@ app.post('/api/persons', (request, response, next) => {
   });
 
   person.save()
-  .then(createdPerson => {
-    response.json(createdPerson.toJSON());
-  })
-  .catch(error => next(error));
-  
+    .then((createdPerson) => {
+      response.json(createdPerson.toJSON());
+    })
+    .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { body } = request;
 
   const person = {
     name: body.name,
     number: body.number,
-  }
+  };
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedPerson => {
-      response.json(updatedPerson.toJSON())
+    .then((updatedPerson) => {
+      response.json(updatedPerson.toJSON());
     })
     .catch(error => next(error));
-})
+});
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-  .then(() => {
-    console.log('deleted');
-    response.status(204).end();
-  })
-  .catch(error => next(error));
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 const PORT = process.env.PORT;
@@ -109,13 +114,15 @@ app.use(unknownEndpoint);
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
-  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
+  }
+
+  if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message });
   }
 
   next(error);
-}
+};
 
 app.use(errorHandler);
